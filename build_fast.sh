2f01dc1e6fa2c57a1d7a3196b7e9d7751a9688ec
Jason Hsu <jhsu802701@jasonhsu.com>
2019-04-02 16:56:03 +0000

    build_fast.sh successfully runs database migrations

--- build_fast.sh
+++ build_fast.sh
@@ -1,18 +1,14 @@
 #!/bin/bash
 
-sh pg-restart.sh
+echo '---------------------'
+echo 'gem uninstall bundler'
+gem uninstall bundler
 
-# echo '----------------------------'
-# echo 'gem install bundler -v 1.7.3'
-# gem install bundler -v 1.7.3
+echo '-----------------------------'
+echo 'gem install bundler -v 1.17.3'
+gem install bundler -v 1.17.3
 
-# echo '----------------------------'
-# echo 'gem install byebug -v 11.0.1'
-# gem install byebug -v 11.0.1
-
-# echo '--------------------------'
-# echo 'gem install puma -v 3.12.1'
-# gem install puma -v 3.12.1
+sh pg_setup.sh
 
 sh kill_spring.sh
 sh all.sh
--- test_app.sh
+++ test_app.sh
@@ -1,22 +1,14 @@
 #!/bin/bash
 
-echo '---------------------'
-echo 'gem uninstall bundler'
-gem uninstall bundler
-
-echo '-----------------------------'
-echo 'gem install bundler -v 1.17.3'
-gem install bundler -v 1.17.3
-
 sh pg-restart.sh
 
 echo '--------------'
 echo 'bundle install'
 bundle install
 
-# echo '----------------'
-# echo 'rails db:migrate'
-# rails db:migrate
+echo '----------------'
+echo 'rails db:migrate'
+rails db:migrate
 
 # NOTE: The Spring server may distort the test coverage results from MiniTest and SimpleCov.
 # echo '---------------------------'
